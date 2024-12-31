713a18016fa218de678854315ed61fe25a01436c
screensquad <zeroscreensquad@gmail.com>
2024-12-31 13:32:16 +0530

    updated header package

---
+++ header/src/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/products">Products</a>
+          </li>
+          <li>
+            <a href="/cart">Cart</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ header/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "header",
+  exposes: {
+    "./Header": "./src/Header",
+  },
+  // Other configurations...
+};
